2b18f1c2148c28a21f9cd6dae0efeed09d43aedf
axel-op <49279289+axel-op@users.noreply.github.com>
2019-09-30 05:21:15 +0200

    Retrieving commit SHA from context

--- app/lib/event.dart
+++ app/lib/event.dart
@@ -4,39 +4,31 @@ Event getEvent(Map<String, dynamic> payload) {
   final bool isPullRequest = payload.containsKey('pull_request');
   final String repoId = payload['repository']['id'].toString();
   final String repoSlug = payload['repository']['full_name'];
-  final String commitId = payload['after'];
   final int number = isPullRequest ? payload['pull_request']['number'] : null;
   return isPullRequest
       ? PullRequest._(
           number: number,
-          commitId: commitId,
           repoId: repoId,
           repoSlug: repoSlug,
         )
       : Push._(
-          commitId: commitId,
           repoId: repoId,
           repoSlug: repoSlug,
         );
 }
 
 class Event {
-  final String commitId;
   final String repoId;
   final String repoSlug;
 
-  Event._(
-      {@required this.commitId,
-      @required this.repoId,
-      @required this.repoSlug});
+  Event._({@required this.repoId, @required this.repoSlug});
 }
 
 class Push extends Event {
   Push._({
-    @required String commitId,
     @required String repoId,
     @required String repoSlug,
-  }) : super._(commitId: commitId, repoId: repoId, repoSlug: repoSlug);
+  }) : super._(repoId: repoId, repoSlug: repoSlug);
 }
 
 class PullRequest extends Event {
@@ -44,11 +36,9 @@ class PullRequest extends Event {
 
   PullRequest._({
     @required this.number,
-    @required String commitId,
     @required String repoId,
     @required String repoSlug,
   }) : super._(
-          commitId: commitId,
           repoId: repoId,
           repoSlug: repoSlug,
         );
