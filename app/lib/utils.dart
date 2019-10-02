177922ce1c132b2a09489e49f41578326df971fb
axel-op <49279289+axel-op@users.noreply.github.com>
2019-10-02 21:35:04 +0200

    Edited file organization + editd API call for line comments

--- app/lib/utils.dart
+++ app/lib/utils.dart
@@ -1,34 +1,5 @@
 import 'package:app/event.dart';
 import 'package:app/result.dart';
-import 'package:github/server.dart' hide Event;
-import 'package:meta/meta.dart';
-
-/// Post the comment as a commit comment on GitHub
-Future<void> postCommitComment(
-    String comment,
-    {@required
-        final Event event,
-    @required
-        final String commitSha,
-    @required
-        final String githubToken,
-    final int lineNumber,
-    final String fileRelativePath,
-    @required
-        Future<void> Function(dynamic error, dynamic stack) onError}) async {
-  try {
-    final GitHub github =
-        createGitHubClient(auth: Authentication.withToken(githubToken));
-    final Repository repo = await github.repositories
-        .getRepository(RepositorySlug.full(event.repoSlug));
-    final RepositoryCommit commit =
-        await github.repositories.getCommit(repo.slug(), commitSha);
-    await github.repositories.createCommitComment(repo.slug(), commit,
-        body: comment, path: fileRelativePath, position: lineNumber);
-  } catch (e, s) {
-    await onError(e, s);
-  }
-}
 
 /// Build message to be posted on GitHub
 String buildComment(Result result, Event event, String commitSha) {
@@ -67,7 +38,7 @@ String _stringSuggestion(Suggestion suggestion) {
     str += '**: ';
   }
   ;
-  str += suggestion.description.replaceAll(RegExp(r'(\n)+'), '\n  *');
+  str += suggestion.description.replaceAll(RegExp(r'(\n)+'), '\n  * ');
   return str;
 }
 
