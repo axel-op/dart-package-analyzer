93257d88d15a010b140e3cb7a7cba8c61b61a3de
Axel Ogereau-Peltier <49279289+axel-op@users.noreply.github.com>
2020-05-11 01:32:55 +0200

    Edit paths

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ const exec = require('@actions/exec');
 
 async function run() {
     try {
-        const home = process.env.HOME;
+        const flutterHome = process.env.FLUTTER_HOME;
         const workspace = process.env.GITHUB_WORKSPACE;
-        await core.addPath(`${home}/flutter/bin`);
-        await core.addPath(`${home}/flutter/bin/cache/dart-sdk/bin`);
-        await core.addPath(`${home}/.pub-cache/bin`);
+        await core.addPath(`${flutterHome}/.pub-cache/bin`);
         await exec.exec('flutter', ['pub', 'global', 'activate', 'pana']);
         await exec.exec('flutter', ['run', `${workspace}/app/bin/main.dart`]);
     }
